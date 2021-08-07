c5076d3fe8c06cd22a779c4b4c369869fed62cb4
programmer1024r <reempro112@gmail.com>
2021-08-07 10:27:03 +0300

    Added the Disappeared Numbers in Go language

---
+++ DisappearedNumbersInArray.go
@@ -0,0 +1,26 @@
+/*
+Use: find the missing numbers in array with range [1, n], n = array length
+Input: array pointer, and the array size
+Output: the missing numbers
+ */
+func findDisappearedNumbers(nums []int) []int {
+    
+    n := len(nums)
+    hashTable :=  make(map[int]int) // The key will be the value
+    var solution []int
+    
+    // Put nums in hash table 
+    for i := 0; i < n ; i++ {
+        hashTable[nums[i]] = nums[i]   
+    }
+    
+    // go throw numbers from 1 - n (len(nums)) and check if they are in the hash table
+    for i := 1; i <= n; i++ {
+        // the number is not in the hash table
+        if hashTable[i] == 0 {
+            solution = append(solution, i)       
+        } 
+    }
+    
+    return solution
+}
